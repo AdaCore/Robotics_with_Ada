3e84b8683c05216caf8e28506422becf92228089
rogers <rogers@adacore.com>
2020-01-11 12:31:52 -0600

    add postconditions and their necessary preconditions

--- src/utils/math_utilities.ads
+++ src/utils/math_utilities.ads
@@ -35,24 +35,36 @@ package Math_Utilities is
 
    generic
       type T is range <>;
-   function Range_To_Domain_Mapping (Value, Range_Min, Range_Max, Domain_Min, Domain_Max : T)
-   return T
-   with Inline;
+   function Range_To_Domain_Mapping
+     (Value, Range_Min, Range_Max, Domain_Min, Domain_Max : T)
+      return T
+   with
+     Pre  => Range_Min < Range_Max and Domain_Min < Domain_Max,
+     Post => Range_To_Domain_Mapping'Result in Domain_Min .. Domain_Max,
+     Inline;
    --  Maps the Value, with the range Range_Min .. Range_Max, to a value in the
    --  domain Domain_Min .. Domain_Max
 
    generic
       type T is range <>;
-   function Bounded_Integer_Value (Value, Low, High : T)
-   return T
-   with Inline;
+   function Bounded_Integer_Value
+     (Value, Low, High : T)
+      return T
+   with
+     Pre  => Low < High,
+     Post => Bounded_Integer_Value'Result in Low .. High,
+     Inline;
    --  Constrains the input value to the range Low .. High
 
    generic
       type T is digits <>;
-   function Bounded_Floating_Value (Value, Low, High : T)
-   return T
-   with Inline;
+   function Bounded_Floating_Value
+     (Value, Low, High : T)
+      return T
+   with
+     Pre  => Low < High,
+     Post => Bounded_Floating_Value'Result in Low .. High,
+     Inline;
    --  Constrains the input value to the range Low .. High
 
 end Math_Utilities;
