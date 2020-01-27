7bf8500011ac18acc12a4ee6a9db38c870bd313f
rogers <rogers@adacore.com>
2020-01-27 07:17:36 -0600

    comment formatting

--- src/utils/task_last_wishes.ads
+++ src/utils/task_last_wishes.ads
@@ -1,4 +1,6 @@
---  This package defines what happens when a task terminates, which is unexpected in Ravenscar-based profiles. In this case the last chance handler is invoked via Program_Error with a string indicating which task terminated.
+--  This package defines what happens when a task terminates, which is
+--  unexpected in Ravenscar-based profiles. In this case the last chance handler
+--  is invoked via Program_Error with a string indicating which task terminated.
 
 with Ada.Task_Identification;
 
