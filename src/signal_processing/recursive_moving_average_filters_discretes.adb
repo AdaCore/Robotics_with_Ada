c69e87894c04d2eaf67175d8244639daa3ae8de4
rogers <rogers@adacore.com>
2020-01-30 22:48:08 -0600

    now at Silver level

--- src/signal_processing/recursive_moving_average_filters_discretes.adb
+++ src/signal_processing/recursive_moving_average_filters_discretes.adb
@@ -1,4 +1,12 @@
+with Math_Utilities;
+
 package body Recursive_Moving_Average_Filters_Discretes is
+
+   procedure Add (Value : Sample; To : in out Accumulator) with Inline;
+   --  Add sample (which can be negative) to To, without overflowing
+
+   procedure Subtract (Value : Sample; From : in out Accumulator) with Inline;
+   --  Subtract sample (which can be negative) from From, without overflowing
 
    -----------
    -- Value --
@@ -7,37 +15,88 @@ package body Recursive_Moving_Average_Filters_Discretes is
    function Value (This : RMA_Filter) return Sample is
      (This.Averaged_Value);
 
+   -----------
+   -- Limit --
+   -----------
+
+   procedure Limit is new Math_Utilities.Bound_Integer_Value (T => Accumulator);
+
    ------------
    -- Insert --
    ------------
 
    procedure Insert (This : in out RMA_Filter;  New_Sample : Sample) is
+      Average : Accumulator;
    begin
       if Empty (This.Samples) then
          Put (This.Samples, New_Sample);
          This.Total := Accumulator (New_Sample);
-         This.Averaged_Value := Sample (This.Total);
+         This.Averaged_Value := New_Sample;
          return;
       end if;
 
-      if Full (This.Samples) then -- delete the oldest sample and update total
+      if Full (This.Samples) then
+         --  Delete the oldest sample and remove its value from total (so that
+         --  we never have to iterate over all the samples in order to calculate
+         --  a new total.
          declare
             Oldest : Sample;
          begin
             Get (This.Samples, Oldest);
-            This.Total := This.Total - Accumulator (Oldest);
+            Subtract (Oldest, From => This.Total);
          end;
       end if;
 
       Put (This.Samples, New_Sample);
 
-      This.Total := This.Total + Accumulator (New_Sample);
-      --  The above potentially overflows, but there is no issue of accumulating
-      --  round-off errors over time, unlike what would happen if we used a
-      --  floating point type.
+      Add (New_Sample, To => This.Total);
 
-      This.Averaged_Value := Sample (This.Total / Accumulator (Extent (This.Samples)));
+      Average := This.Total / Accumulator (Extent (This.Samples));
+      Limit (Average, Accumulator (Sample'First), Accumulator (Sample'Last));
+      This.Averaged_Value := Sample (Average);
    end Insert;
+
+   ---------
+   -- Add --
+   ---------
+
+   procedure Add (Value : Sample; To : in out Accumulator) is
+   begin
+      if Value > 0 then
+         if To <= Accumulator'Last - Accumulator (Value) then
+            To := To + Accumulator (Value);
+         else
+            To := Accumulator'Last;
+         end if;
+      else -- Value is negative (or zero)
+         if To >= Accumulator'First - Accumulator (Value) then
+            To := To + Accumulator (Value);
+         else
+            To := Accumulator'First;
+         end if;
+      end if;
+   end Add;
+
+   --------------
+   -- Subtract --
+   --------------
+
+   procedure Subtract  (Value : Sample; From : in out Accumulator) is
+   begin
+      if Value > 0 then
+         if From >= Accumulator'First + Accumulator (Value) then
+            From := From - Accumulator (Value);
+         else
+            From := Accumulator'First;
+         end if;
+      else -- Value is negative (or zero)
+         if From <= Accumulator'Last + Accumulator (Value) then
+            From := From - Accumulator (Value);
+         else
+            From := Accumulator'Last;
+         end if;
+      end if;
+   end Subtract;
 
    -----------
    -- Reset --
--- src/signal_processing/recursive_moving_average_filters_discretes.ads
+++ src/signal_processing/recursive_moving_average_filters_discretes.ads
@@ -25,27 +25,38 @@ generic
 package Recursive_Moving_Average_Filters_Discretes is
 
    pragma Compile_Time_Error
-      (Accumulator (Sample'First) < Accumulator'First or
-       Accumulator (Sample'Last) > Accumulator'Last,
-       "Accumulator range is insufficient for Sample");
+      (Sample'Size * 2 > Accumulator'Size,
+       "Accumulator size should be twice that of Sample");
 
-   type RMA_Filter (Window_Size : Positive) is tagged limited private;
+   pragma Compile_Time_Error
+      (Sample'First >= Sample'Last or
+       Accumulator'First >= Accumulator'Last,
+       "Accumulator and Sample ranges must not be null");
+
+   subtype Filter_Window_Size is Integer range 1 .. Integer'Last / 2;
+
+   type RMA_Filter (Window_Size : Filter_Window_Size) is tagged limited private;
 
    procedure Insert (This : in out RMA_Filter;  New_Sample : Sample);
+   --  Updates the new average value based on the value of New_Sample
 
    function Value (This : RMA_Filter) return Sample with Inline;
+   --  simply returns the average value previously computed by Insert
 
-   procedure Reset (This : out RMA_Filter);
+   procedure Reset (This : out RMA_Filter) with
+     Post'Class => Value (This) = 0;
 
 private
 
    package Sample_Data is new Sequential_Bounded_Buffers (Element => Sample, Default_Value => 0);
    use Sample_Data;
 
-   type RMA_Filter (Window_Size : Positive) is tagged limited record
+   type RMA_Filter (Window_Size : Filter_Window_Size) is tagged limited record
       Samples        : Sample_Data.Ring_Buffer (Window_Size);
       Averaged_Value : Sample := 0;
-      Total          : Accumulator;
+      Total          : Accumulator := 0;
+      --  There is no issue of accumulating round-off errors over time, unlike
+      --  what would happen if we used a floating point type for the Total
    end record;
 
 end Recursive_Moving_Average_Filters_Discretes;
